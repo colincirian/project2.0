ee73b98f9b59189beb3b1477baa46eafd2071f5d
Colin <colindcirian@gmail.com>
2023-09-02 13:24:58 -0700

    added a few pages

---
+++ src/pages/home.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div className = "screen">
+            <h1>aim trainer</h1>
+            <section className = "div-container">
+                <div>
+                    <a href="/easy">Easy</a>
+                </div>
+
+                <div>
+                    
+                </div>
+
+                <div>
+                    
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Home;
