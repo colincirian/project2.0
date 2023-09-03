a6585e753e875cbbd2918eafc6bc666f266d7445
Colin <colindcirian@gmail.com>
2023-09-02 20:56:21 -0700

    fixed screen issue with css

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,22 +1,10 @@
 import React from "react";
 
 function Home() {
+
     return (
-        <div className = "screen">
-            <h1>aim trainer</h1>
-            <section className = "div-container">
-                <div>
-                    <a href="/easy">Easy</a>
-                </div>
-
-                <div>
-                    
-                </div>
-
-                <div>
-                    
-                </div>
-            </section>
+        <div className="pages">
+           <h1>Aim Trainer</h1>
         </div>
     )
 }
